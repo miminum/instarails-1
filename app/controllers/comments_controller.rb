9137094c2c8ca4e66fea1a6e0ea09d2e0442585f
Daniel Huong <daniel.huong86@gmail.com>
2017-09-27 14:49:17 +1000

    only photo's comments can be seen against each photo

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -4,7 +4,8 @@ class CommentsController < ApplicationController
   # GET /comments
   # GET /comments.json
   def index
-    @comments = Comment.all
+    # @comments = Comment.where(photo: @photo)
+    @comments = @photo.comments
     @new_comment = Comment.new
   end
 
